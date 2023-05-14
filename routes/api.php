e77b5584b0fc08d419ebbc43a25e442a94196e11
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-05-14 19:57:43 +1200

    Updated the api routes

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,5 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
 
@@ -14,11 +13,6 @@ use App\Http\Controllers\ProductController;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
 
 Route::get('/product/all', [ProductController::class, 'getAllProducts']);
 
